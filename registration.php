1fa894c0092769c57318d36c36243bb12ceb4e2b
Luke Paoloni <designcode.luke@gmail.com>
2018-02-06 00:06:32 +0000

    Init of theme

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Luke Paoloni, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Zeus/Alexus',
+    __DIR__
+);
