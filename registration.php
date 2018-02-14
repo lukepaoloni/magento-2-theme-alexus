c433e8d2b335395e6415b867fb966027b64f496e
Luke Paoloni <designcode.luke@gmail.com>
2018-02-14 19:14:55 +0000

    Init theme config

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Zeus/Alexus',
+    'frontend/Zeus/alexus',
     __DIR__
 );
